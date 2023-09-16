674aa25ae46ab93a97805dbf78546f7fd439c46f
SolomonChidera <solochidera10@gmail.com>
2023-09-16 23:56:25 +0100

    Header file with updated prototypes

--- main.h
+++ main.h
@@ -6,8 +6,11 @@
 #include <stdarg.h>
 #include <unistd.h>
 int _pu(*st);
+int print_s(va_list ab, ...);
+int print_char(va_list ab, ...);
+int percent(va_list ab, ...);
 int pear(char c);
 int _printf(const char *format, ...);
-
+int point_fun(char *s, va_list ab, *parm);
 
 #endif
