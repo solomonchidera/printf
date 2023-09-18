13e9602567226b0ba61a3d87f4d77f68314a514c
Moniaar <www.omniaahmed1562001@gmail.com>
2023-09-18 03:00:45 -0700

    our tested header files and the main.c

--- main.h
+++ main.h
@@ -2,19 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.>
+#include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
-int _pu(*st);
-int print_s(va_list ab, ...);
-int print_char(va_list ab, ...);
-int percent(va_list ab, ...);
-int pear(char c);
-int _printf(const char *format, ...);
-<<<<<<< HEAD
-int pcar(char c);
-=======
-int point_fun(char *s, va_list ab, *parm);
->>>>>>> 485403dbce687e37f8e7844a42b644283c891782
+int putcar(char c);
+int print_string(const char *format, ...);
 
 #endif
