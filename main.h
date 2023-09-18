e2235d4e7fd5a23ec2d858d5ee933d951fd56e5a
SolomonChidera <solochidera10@gmail.com>
2023-09-18 21:31:02 +0100

    Am tired :((((((((

--- main.h
+++ main.h
@@ -8,7 +8,6 @@
 void _printf_s(const char *s, int *char_count);
 void _printf_c(int c, int *char_count);
 int _printf(const char *format, ...);
-int _printf2(const char *format, ...);
 int _puts(char *c);
 int putcar(char c);
 
