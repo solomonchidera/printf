0ea040a0fe763e468237acc1e876b92e1de3a1ef
SolomonChidera <solochidera10@gmail.com>
2023-09-18 11:17:13 +0100

    Removed it entirely :)

--- main.c
+++ main.c
@@ -9,14 +9,11 @@
  */
 int main(void)
 {
-	int len;
-	(void)int len2;
-	(void)unsigned int ui;
-	(void)void *addr;
+	
 
-	len = print_string("Let's try to printf a simple sentence.\n");
+	print_string("Let's try to printf a simple sentence.\n");
 	print_string("Character:[%c]\n", 'H');
 	print_string("String:[%s]\n", "I am a string !");
-	len = print_string("Percent:[%%]\n");
+	print_string("Percent:[%%]\n");
 	return (0);
 }
