627e1f14d2742a2181bd95d88e4bcd357cc287ae
SolomonChidera <solochidera10@gmail.com>
2023-09-18 11:05:38 +0100

    Removed version control conflict marker in file[main.h]

--- main.h
+++ main.h
@@ -5,26 +5,8 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
-<<<<<<< HEAD
+
 int putcar(char c);
 int print_string(const char *format, ...);
-=======
-<<<<<<< HEAD
-/*int percent_print(const char *format, ...);*/
-int print_st(const char *format, ...);
-=======
-int _pu(*st);
-int print_s(va_list ab, ...);
-int print_char(va_list ab, ...);
-int percent(va_list ab, ...);
-int pear(char c);
-int _printf(const char *format, ...);
-<<<<<<< HEAD
-int pcar(char c);
-=======
-int point_fun(char *s, va_list ab, *parm);
->>>>>>> 485403dbce687e37f8e7844a42b644283c891782
->>>>>>> 05b68dfae435067902e0ce50432971267cf7fd42
->>>>>>> 022cc7df245102b0b22b0579b9e9fb556a1b16d9
 
 #endif
