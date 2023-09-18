b970bff83facbc26a6f24844e77ce78c090dc752
SolomonChidera <solochidera10@gmail.com>
2023-09-18 11:23:20 +0100

    Corrected an error[*format == '%']

--- percent_percent.c
+++ percent_percent.c
@@ -1,12 +1,13 @@
-include "main.h"
+#include "main.h"
 
 int print_percent(const char *format, ...)
 {
 
-if (format == "%%")
+if (*format == '%')
 	putchar(37 + '0');
 	else
 	{
 		putchar(*format);
 	}
+	return (0);
 }
