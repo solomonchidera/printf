829e01423bfafadbf43e7345ab6d6d66db36b912
SolomonChidera <solochidera10@gmail.com>
2023-09-18 11:14:15 +0100

    Voided some unused variables

--- main.c
+++ main.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
+	int len;
+	(void)int len2;
+	(void)unsigned int ui;
+	(void)void *addr;
 
-    len = print_string("Let's try to printf a simple sentence.\n");
-    print_string("Character:[%c]\n", 'H');
-    print_string("String:[%s]\n", "I am a string !");
-    len = print_string("Percent:[%%]\n");
-    return (0);
+	len = print_string("Let's try to printf a simple sentence.\n");
+	print_string("Character:[%c]\n", 'H');
+	print_string("String:[%s]\n", "I am a string !");
+	len = print_string("Percent:[%%]\n");
+	return (0);
 }
