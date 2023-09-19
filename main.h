2c638dcb5428910b2970d30ebb58f2be87f9fcd2
Moniaar <www.omniaahmed1562001@gmail.com>
2023-09-19 14:32:16 -0700

    adding our updated main.h

--- main.h
+++ main.h
@@ -10,5 +10,7 @@ int printdigit(const char *format, ...);
 int _putchar(char c);
 void _printf_s(const char *s, int *char_count);
 void _printf_c(int c, int *char_count);
+int percent_print(const char *format, ...);
+int _printf(const char *format, ...);
 
 #endif
