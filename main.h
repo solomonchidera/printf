5ec85b842393b1e2008d7ace6b750604a686d938
SolomonChidera <solochidera10@gmail.com>
2023-09-20 19:38:55 +0100

    [#53] Merge issue solved

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type of element
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *type;
+	int (*func)(va_list);
+} print_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_d(va_list d);
+int print_i(va_list i);
+
+
+#endif
